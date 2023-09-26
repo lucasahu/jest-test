21b16f75ccbafaa48c2905cd5007309fbaca6fe7
lucas ubuntu <lucasignahumada@gmail.com>
2023-09-26 12:22:09 -0300

    Add finished functions and corresponding tests

--- functions.js
+++ functions.js
@@ -22,20 +22,58 @@ const calculator = {
 }
 
 function caesarCipher (string, shift) {
-    //todo
+    let characters = string.split('');
+    let encryptedSentence = [];
+    for (let i = 0; i < characters.length; i++) {
+        let charCode = characters[i].charCodeAt(0);
+        let baseShift = shift;
+            
+        if (charCode <= 90 && charCode >= 65 && charCode + baseShift > 90) {
+            baseShift = shift - (91 - charCode)
+            charCode = 65
+        }
+        
+        if (charCode <= 122 && charCode >= 97 && charCode + baseShift > 122) {
+            baseShift = shift - (123 - charCode)
+            charCode = 97
+        }
+
+        let encryptedChar = String.fromCharCode(charCode + baseShift);
+        encryptedSentence.push(encryptedChar);
+    }
+    return encryptedSentence.join('')
 }
 
 function analyzeArray (array) {
+    let total = 0;
+    let min = array[0];
+    let max = 0;
+    let length = array.length;
+
+    for (let i = 0; i < array.length; i++) {
+        total += array[i];
+        if (array[i] <= min) {
+            min = array[i]
+        } else if (array[i] >= max) {
+            max = array[i]
+        }
+    }
+
+    let average = Math.round(total / length);
+
+
     return {
-        average: null,
-        min: null,
-        max: null,
-        length: null
+        average: average,
+        min: min,
+        max: max,
+        length: length
     }
 }
 
 module.exports = {
     capitalize: capitalize,
     reverseString: reverseString,
-    calculator: calculator
+    calculator: calculator,
+    caesarCipher: caesarCipher,
+    analyzeArray: analyzeArray
 }
--- functions.test.js
+++ functions.test.js
@@ -23,3 +23,24 @@ test ('Calculator multiplies numbers correctly', () => {
 test ('Calculator divides numbers correctly', () => {
     expect(functions.calculator.divide(15, 3)).toBe(5);
 })
+
+test ('Shifts strings', () => {
+    expect(functions.caesarCipher('hello', 3)).toBe('khoor');
+})
+
+test ('Shift wraps correctly upper case chars', () => {
+    expect(functions.caesarCipher('HELLO', 25)).toBe('GDKKN');
+})
+
+test ('Shift wraps correctly lower case chars', () => {
+    expect(functions.caesarCipher('hello', 25)).toBe('gdkkn');
+})
+
+test ('Analyze array works', () => {
+    expect(functions.analyzeArray([1,8,3,4,2,6])).toEqual({
+        average: 4,
+        min: 1,
+        max: 8,
+        length: 6
+    });
+})
